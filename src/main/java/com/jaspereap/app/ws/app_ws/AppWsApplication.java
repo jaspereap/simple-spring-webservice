edf64f7a1ecb20f34998eb8979e6a1c0b59e6ad4
jaspereap <jaspereap@gmail.com>
2024-08-30 18:50:26 +0800

    Initial

---
+++ src/main/java/com/jaspereap/app/ws/app_ws/AppWsApplication.java
@@ -0,0 +1,13 @@
+package com.jaspereap.app.ws.app_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaspereap/app/ws/app_ws/AppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaspereap.app.ws.app_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
